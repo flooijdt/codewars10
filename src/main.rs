d2f538e3158f5b0c0f15fd2e8c8ce50e13dc47f7
flooijdt <flooijdt@posteo.de>
2023-02-18 10:53:59 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn move_zeros(arr: &[u8]) -> Vec<u8> {
+    let mut vecky = arr.to_vec();
+    let mut to_remove: Vec<usize> = Vec::new();
+    for i in vecky.iter().enumerate() {
+        if i.1 == '0' {
+            to_remove.push(i.0);
+        }
+    }
+    let mut counter = 0;
+    for i in to_remove {
+        vecky.remove(i - counter);
+        vecky.push(0);
+        counter += 1;
+    }
+}
